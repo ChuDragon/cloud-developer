fe81c28745bc54621ac6471de998e3a717a98bee
Roman Chuyan <92877535+ChuDragon@users.noreply.github.com>
2022-01-05 18:03:08 -0500

    Rest API course exercises

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,19 +1,25 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": process.env.POSTGRESS_USERNAME,
+    "password": process.env.POSTGRESS_PASSWORD,
+    "database": process.env.POSTGRESS_DATABASE,
+    "host": process.env.POSTGRESS_HOST,
     "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
-  },
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET,
+    "aws_access_id": process.env.AWS_ACESS_ID,
+    "aws_access_key_id": process.env.AWS_ACESS_KEY_ID,
+    "aws_secret_access_key": process.env.AWS_SECRET_ACESS_KEY
+},
   "prod": {
     "username": "",
     "password": "",
     "database": "udagram_prod",
     "host": "",
     "dialect": "postgres"
-  }
+  },
+  "jwt": {
+    "secret": process.env.JWT_SECRET
+  } 
 }
